de753055cf789a25091bc25f4d894e31ff650ee2
RaposoBrendo <raposobrendo@gmail.com>
2025-06-16 17:10:42 -0300

    First commit

---
+++ JS/01 - Beggining/comentario.js
@@ -0,0 +1,13 @@
+// Um comentário
+console.log('Brendo') //Podemos comentar em qualquer lugar
+/* Comentário em 
+múltiplas linhas ;)*/
+
+/*
+ * console.log('As vezes o sistema
+ * entende que
+ * queremos dividir em
+ * várias linhas 
+ * e coloca o asterisco
+ * automaticamente') 
+ */
---
+++ JS/01 - Beggining/sentenca_1.js
@@ -0,0 +1,1 @@
+console.log('Brendo')
