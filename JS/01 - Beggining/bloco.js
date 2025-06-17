c447976ee7eba3403d75bbb84c6ec81ef2cb053d
RaposoBrendo <raposobrendo@gmail.com>
2025-06-17 16:58:10 -0300

    code blocks

---
+++ JS/01 - Beggining/bloco.js
@@ -0,0 +1,13 @@
+{
+    console.log('Passo #01')
+    console.log('Passo #02')
+    console.log('Passo #03')
+    console.log('Fim do bloco #01')
+}
+
+{
+    console.log('Passo #04')
+    console.log('Passo #05')
+    console.log('Passo #06')
+    console.log('Fim do bloco #02')
+}
